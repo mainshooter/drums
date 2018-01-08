efaf03e83013b4c7320502d61a289ee6e90ebeb1
Peter 4A5A <498883@edu.rocmn.nl>
2018-01-08 13:59:22 +0100

    Optimalisation

--- drum-kit.js
+++ drum-kit.js
@@ -1,4 +1,3 @@
-
 var sounds = {
   'a':'clap',
   's': 'hihat',
@@ -17,7 +16,7 @@ var sounds = {
  */
 (function() {
   document.addEventListener('keydown', function() {
-    drumHandler(event)
+    drumHandler(event);
   });
 })();
 
@@ -26,14 +25,13 @@ var sounds = {
  * @param  {[obj]} event [The event object]
  */
 function drumHandler(event) {
+  let keyLetter = event.key.toLowerCase();
   if (sounds[event.key] != null) {
     playSound('sounds/' + sounds[event.key] + '.wav');
     highlight_key(event.which);
     setTimeout(function() {
       disable_highlite(event.which);
-      console.log('done');
     }, 100);
-
   }
 }
 
@@ -41,8 +39,8 @@ function drumHandler(event) {
  * Highlight the key
  * @param  {[int]} key_code [The code of the key]
  */
-function highlight_key(key_code) {
-  let key = document.querySelector('div[data-key="' + key_code + '"]');
+function highlight_key(keyCode) {
+  let key = document.querySelector('div[data-key="' + keyCode + '"]');
     key.className = 'key playing';
 }
 
@@ -50,8 +48,8 @@ function highlight_key(key_code) {
  * Removes the highlight from a key
  * @param  {[int]} key_code [The code of a key]
  */
-function disable_highlite(key_code) {
-  let key = document.querySelector('div[data-key="' + key_code + '"]');
+function disable_highlite(keyCode) {
+  let key = document.querySelector('div[data-key="' + keyCode + '"]');
     key.className = 'key';
 }
 
@@ -59,7 +57,7 @@ function disable_highlite(key_code) {
  * Plays the sound of the audio file
  * @param  {[string]} audio_file [The location of the audio file]
  */
-function playSound(audio_file) {
-    let audio = new Audio(audio_file);
+function playSound(audioFile) {
+    let audio = new Audio(audioFile);
     audio.play();
 }
